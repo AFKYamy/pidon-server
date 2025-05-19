81b334cc5c9df8c4dc5c8aa47e6780a447d31298
Honza <yamy956@proton.me>
2025-05-19 10:57:03 +0200

    Fix image paths in Home component for GitHub Pages

--- src/Components/LevelLeaderboard.js
+++ src/Components/LevelLeaderboard.js
@@ -11,11 +11,11 @@ export default function LevelLeaderboard() {
             <h3>
                 Level Leaderboard
             </h3>
-            <LevelPlayer img={process.env.PUBLIC_URL + '/Images/devil-head.png'} name="DevilNo9" level="3339" />
+            <LevelPlayer img="/Images/devil-head.png" name="DevilNo9" level="3339" />
             <span></span>
-            <LevelPlayer img={process.env.PUBLIC_URL + '/Images/gemtik-head.png'} name="GEMTIkcz" level="229" />
+            <LevelPlayer img="/Images/gemtik-head.png" name="GEMTIkcz" level="229" />
             <span></span>
-            <LevelPlayer img={process.env.PUBLIC_URL + '/Images/manie-head.png'} name="Manie_cz" level="19" />
+            <LevelPlayer img="/Images/manie-head.png" name="Manie_cz" level="19" />
             <Link to="/Leaderboards">
               <Button id="level-leaderboard__btn" text="Show leaderboard" type="btn--secondary" style={{marginTop: "100em"}}/>
             </Link>
